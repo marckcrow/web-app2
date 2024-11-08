96315bfa312681ced33e0c4943772d12bccdd670
marckcrow <marcondesjr.ti@gmail.com>
2024-11-08 19:05:38 -0300

    Atualizado pelo notebook_house

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -1,4 +1,4 @@
 const api = axios.create({
-    baseURL: 'http://localhost:5000', // Altere para o URL do seu backend
+    baseURL: 'http://localhost:5001', // Altere a porta se necessário
   });
   
